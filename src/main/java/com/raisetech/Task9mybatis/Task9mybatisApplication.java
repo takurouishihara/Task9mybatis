a171daa396c2d58bafb0761681f45cdca787e7d3
takurouishihara <k19008@std.nagaokauniv.ac.jp>
2022-11-04 22:38:09 +0900

    プロジェクトのセットアップ

---
+++ src/main/java/com/raisetech/Task9mybatis/Task9mybatisApplication.java
@@ -0,0 +1,13 @@
+package com.raisetech.Task9mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task9mybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task9mybatisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raisetech/Task9mybatis/Task9mybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raisetech.Task9mybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task9mybatisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
